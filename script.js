d51cfaf841d30449af61de2bbaa4f5bb1808cb40
theimpostor <theimpostor@users.noreply.github.com>
2022-10-16 11:45:26 -0500

    add buttons

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-const c = document.getElementById('canvas').getContext('2d')
+const c = document.getElementById("canvas").getContext("2d");
+const bA = document.getElementById("bA");
+const bB = document.getElementById("bB");
 
-c.fillStyle = 'black'
-c.strokeRect(0, 0, 500, 400)
+bA.onclick = function (ev) {
+  alert("reset");
+  init();
+};
 
-c.fillStyle = 'blue'
-c.fillRect(10, 10, 480, 380)
+bB.onclick = function (ev) {
+  c.fillText("button B clicked", 250, 300);
+};
 
-c.fillStyle = 'orange'
-c.font = 'small-caps bold 24px sans-serif'
-c.textAlign = 'center'
-c.fillText('Hello code hackerz! Going to save!', 250, 200)
+function init() {
+  c.clearRect(0, 0, c.width, c.height);
+
+  c.fillStyle = "black";
+  c.strokeRect(0, 0, 500, 400);
+
+  c.fillStyle = "blue";
+  c.fillRect(10, 10, 480, 380);
+
+  c.fillStyle = "orange";
+  c.font = "small-caps bold 24px sans-serif";
+  c.textAlign = "center";
+  c.fillText("Hello code hackerz!", 250, 200);
+}
+
+init();
